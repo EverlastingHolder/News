0ebfc91e4ded2e1ec81276a750682a6be2c16ac4
EverlastingHolder <moshkovcevroma@gmail.com>
2023-09-05 20:05:32 +0700

    feat(detail news): add Detail view

---
+++ NewsApp/Features/DetailNews/NewsDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  NewsDetailViewController.swift
+//  NewsApp
+//
+//  Created by Роман Мошковцев on 05.09.2023.
+//
+
+import Foundation
---
+++ NewsApp/Features/News/View/NewsNavigationController.swift
@@ -0,0 +1,8 @@
+//
+//  NewsNavigationController.swift
+//  NewsApp
+//
+//  Created by Роман Мошковцев on 05.09.2023.
+//
+
+import Foundation
